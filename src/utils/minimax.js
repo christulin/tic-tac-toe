c3bf54ed70650d9d50380740381595dc820b5c73
Joshua Bradley <joshuabradley012@gmail.com>
2020-10-23 21:56:59 -0700

    Complete minimax function

--- src/utils/minimax.js
+++ src/utils/minimax.js
@@ -2,7 +2,6 @@ const board = ['', '', '', '', '', '', '', '', ''];
 
 const anyElementsEqual = (array, val) => array.some(element => element === val);
 const allElementsEqual = (array, val) => array.every(element => element === val);
-const hasMovesLeft = board => allElementsEqual(board, '');
 
 const getLines = board => {
   const n = Math.ceil(Math.sqrt(board.length));
@@ -24,7 +23,6 @@ const getLines = board => {
 
     if (!lines.rows[row]) lines.rows[row] = [];
     if (!lines.cols[col]) lines.cols[col] = [];
-
     lines.rows[row].push(move);
     lines.cols[col].push(move);
   }
@@ -50,13 +48,13 @@ const evaluate = (board, player, opponent) => {
   for (let line of lines) {
     if (line[0] === '') continue;
 
-    const currentPlayer = line[0];
-    const isWinner = allElementsEqual(line, currentPlayer);
+    const winningPlayer = line[0];
+    const isWinner = allElementsEqual(line, winningPlayer);
 
     if (isWinner) {
-      if (currentPlayer === player) {
+      if (winningPlayer === player) {
         return 10;
-      } else if (currentPlayer === opponent) {
+      } else if (winningPlayer === opponent) {
         return -10;
       }
     }
@@ -65,8 +63,79 @@ const evaluate = (board, player, opponent) => {
   return 0;
 };
 
-//console.log(evaluate(['x', 'x', '', 'o', 'x', 'o', 'o', 'x', ''], 'x', 'o'));
+const hasMovesLeft = board => anyElementsEqual(board, '');
 
-const minimax = board => {
+const minimax = (board, depth, isMax, player = 'x', opponent = 'o', alpha = Number.NEGATIVE_INFINITY, beta = Number.POSITIVE_INFINITY) => {
+  const score = evaluate(board, player, opponent);
 
+  // If it a winning or losing board, return the score
+  // weighted by depth
+  if (score === 10) {
+    return score - depth;
+  }
+
+  if (score === -10) {
+    return score + depth;
+  }
+
+  // If the game is a draw, return a neutral score
+  if (hasMovesLeft(board)) return 0;
+
+  // Get best score for the maximizer
+  if (isMax) {
+    const best = Number.NEGATIVE_INFINITY;
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        board[i] = player;
+        best = Math.max(best, minimax(board, depth + 1, !isMax, player, opponent, alpha, beta));
+        board[i] = '';
+
+        alpha = Math.max(alpha, best);
+        if (beta <= alpha) break;
+      }
+    }
+    return best;
+
+  // Get the best score for the minimizer
+  } else {
+    const best = Number.POSITIVE_INFINITY;
+
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === '') {
+        board[i] = opponent;
+        best = Math.min(best, minimax(board, depth + 1, !isMax, player, opponent, alpha, beta));
+        board[i] = '';
+
+        beta = Math.min(beta, best);
+        if (beta <= alpha) break;
+      }
+    }
+    return best;
+
+  }
 };
+
+const findBestMove = (board, player = 'x', opponent = 'o') => {
+  let bestVal = Number.NEGATIVE_INFINITY;
+  let bestMove = -1;
+
+  // For every empty cell get the value
+  // to find the highest possible value
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') {
+      board[i] = player;
+      const moveVal = minimax(board, 0, false, player, opponent);
+      board[i] = '';
+
+      if (moveVal > bestVal) {
+        bestMove = i;
+        bestVal = moveVal;
+      }
+    }
+  }
+
+  return bestMove;
+};
+
+export default findBestMove;
