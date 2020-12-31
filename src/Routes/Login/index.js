3fc5152ae8a76cfb1dda74a0958eeaa4f6d2efda
Richard Paul Anderson <debughug@gmail.com>
2020-12-30 16:04:01 -0800

    Login Route: Updated login page to just be a switch between the two games.

--- src/Routes/Login/index.js
+++ src/Routes/Login/index.js
@@ -1,48 +1,28 @@
 import React from 'react';
+import Select from 'react-select';
+import { useHistory } from 'react-router-dom';
 
-export class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Login = ({ gameOptions }) => {
+  let history = useHistory();
 
-  render() {
-    return (
-      <div className="route-login">
-        <div className="sidebar">
-          <h1>Tic Tac Toe</h1>
-        </div>
-        <div className="main">
-          <form>
-            <h2>Sign in to Tic Tac Toe</h2>
-            <fieldset>
-              <label for="login">Username or Email Address</label>
-              <input
-                type="text"
-                name="login"
-                id="login"
-                tabindex="1"
-                class="text-input"
-                autocorrect="off"
-                autocapitalize="off"
-              />
-            </fieldset>
-            <fieldset>
-              <label for="login">Password</label>
-              <input
-                type="text"
-                name="login"
-                id="login"
-                tabindex="1"
-                class="text-input"
-                autocorrect="off"
-                autocapitalize="off"
-              />
-            </fieldset>
-            <input class="button form-sub" type="submit" value="Sign In" tabindex="3"></input>
-          </form>
-        </div>
+  let goToGame = game => {
+    history.push(`/${game.value}`);
+  };
+
+  return (
+    <div className="route-login">
+      <div className="main">
+        <form>
+          <h2>Welcome to Pktgmz</h2>
+          <fieldset>
+            <label htmlFor="game">Pick a game</label>
+            <Select options={gameOptions} onChange={goToGame} className="dropdown" classNamePrefix="dropdown" />
+          </fieldset>
+          <button>Lets Play</button>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Login;
