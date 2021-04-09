61b3017b6bac78e0a55d20ec369d1ef506339e16
Richard Paul Anderson <richard.anderson@procore.com>
2021-04-08 17:16:28 -0700

    Hangman Beta: Time to Refactor :)

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Navbar from './Layouts/NavBar';
 import Login from './Routes/Login';
 import { TicTacToe } from './Routes/TicTacToe';
 import { ConnectFour } from './Routes/ConnectFour';
+import { Hangman } from './Routes/Hangman';
 
 export default function App() {
   const gameOptions = [
     { value: 'tic-tac-toe', label: 'Tic Tac Toe' },
     { value: 'connect-four', label: 'Connect Four' },
+    { value: 'hangman', label: 'Hangman' },
   ];
 
   return (
@@ -25,6 +27,9 @@ export default function App() {
           </Route>
           <Route path="/tic-tac-toe">
             <TicTacToe />
+          </Route>
+          <Route path="/hangman">
+            <Hangman />
           </Route>
         </Switch>
       </div>
